65516ecd5803143d04872e5237e6fa710186aeb2
Nivekithan S <nivekithan363@gmail.com>
2023-09-25 16:45:03 +0530

    Leetcode: 34 Find First and Last Position of Element in Sorted Array

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,85 @@
-pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-    let k_1 = nums[0];
+pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let indexes = binary_search(&nums[0..], target);
 
-    if k_1 == target {
-        return 0;
-    }
-
-    let index = binary_search(&nums[0..], target, k_1);
-
-    match index {
-        None => return -1,
-        Some(v) => return v as i32,
+    match indexes {
+        (Some(l), Some(r)) => return vec![l as i32, r as i32],
+        (None, None) => return vec![-1, -1],
+        _ => unreachable!(),
     };
 
-    fn binary_search(slice: &[i32], target: i32, k_1: i32) -> Option<usize> {
+    fn binary_search(slice: &[i32], target: i32) -> (Option<usize>, Option<usize>) {
         if slice.len() == 0 {
-            return None;
+            return (None, None);
         }
 
         if slice.len() == 1 {
             if slice[0] == target {
-                return Some(0);
+                return (Some(0), Some(0));
             }
 
-            return None;
+            return (None, None);
         }
 
         let low: usize = 0;
         let high = slice.len() - 1;
 
         let middle = find_middle(low, high);
-
         let middle_value = slice[middle];
 
         if middle_value == target {
-            return Some(middle);
+            let is_left_starting = middle == 0 || slice[middle - 1] != target;
+
+            let is_right_starting = middle == slice.len() - 1 || slice[middle + 1] != target;
+
+            println!(
+                "is_left_starting: {is_left_starting}, is_right_starting: {is_right_starting} "
+            );
+
+            if is_right_starting && is_left_starting {
+                return (Some(middle), Some(middle));
+            }
+
+            if is_left_starting && !is_right_starting {
+                let (_, right_index) =
+                    set_index(binary_search(&slice[middle + 1..], target), middle + 1);
+
+                match right_index {
+                    None => unreachable!(),
+                    Some(r) => return (Some(middle), Some(r)),
+                };
+            }
+
+            if !is_left_starting && is_right_starting {
+                let (left_index, _) = binary_search(&slice[0..middle], target);
+
+                println!("{left_index:?}");
+
+                match left_index {
+                    None => unreachable!(),
+                    Some(l) => return (Some(l), Some(middle)),
+                }
+            }
+
+            if !is_left_starting && !is_right_starting {
+                let (left_index, _) = binary_search(&slice[0..middle], target);
+                let (_, right_index) =
+                    set_index(binary_search(&slice[middle + 1..], target), middle + 1);
+
+                match (left_index, right_index) {
+                    (Some(l), Some(r)) => return (Some(l), Some(r)),
+                    _ => unreachable!(),
+                }
+            }
+
+            unreachable!();
         }
 
         if middle_value > target {
-            if middle_value >= k_1 && target > k_1 {
-                return binary_search(&slice[0..middle], target, k_1);
-            }
-
-            if middle_value < k_1 && target > k_1 {
-                unreachable!();
-            }
-
-            if middle_value >= k_1 && target < k_1 {
-                return correct_index(
-                    binary_search(&slice[middle + 1..slice.len()], target, k_1),
-                    middle + 1,
-                );
-            }
-
-            if middle_value < k_1 && target < k_1 {
-                return binary_search(&slice[0..middle], target, k_1);
-            }
+            return binary_search(&slice[0..middle], target);
         }
 
         if middle_value < target {
-            if middle_value >= k_1 && target > k_1 {
-                return correct_index(
-                    binary_search(&slice[middle + 1..slice.len()], target, k_1),
-                    middle + 1,
-                );
-            }
-
-            if middle_value < k_1 && target > k_1 {
-                return binary_search(&slice[0..middle], target, k_1);
-            }
-
-            if middle_value >= k_1 && target < k_1 {
-                unreachable!();
-            }
-
-            if middle_value < k_1 && target < k_1 {
-                return correct_index(
-                    binary_search(&slice[middle + 1..slice.len()], target, k_1),
-                    middle + 1,
-                );
-            }
+            return set_index(binary_search(&slice[middle + 1..], target), middle + 1);
         }
 
         unreachable!();
@@ -86,44 +87,58 @@ pub fn search(nums: Vec<i32>, target: i32) -> i32 {
 
     fn find_middle(low: usize, high: usize) -> usize {
         if (low + high) % 2 == 0 {
-            return low / high;
+            return (low + high) / 2;
         }
 
         return (low + high + 1) / 2;
     }
 
-    fn correct_index(index: Option<usize>, padding: usize) -> Option<usize> {
-        match index {
-            None => None,
-            Some(i) => Some(i + padding),
-        }
+    fn set_index(
+        indexes: (Option<usize>, Option<usize>),
+        padding: usize,
+    ) -> (Option<usize>, Option<usize>) {
+        match indexes {
+            (Some(l), Some(r)) => return (Some(l + padding), Some(r + padding)),
+            (Some(l), None) => return (Some(l + padding), None),
+            (None, Some(r)) => return (None, Some(r + padding)),
+            (None, None) => return (None, None),
+        };
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::search;
+    use crate::search_range;
 
     #[test]
-    fn test() {
-        let input = vec![4, 5, 6, 7, 0, 1, 2];
-        let target = 0;
+    fn test_1() {
+        let input = vec![5, 7, 7, 8, 8, 10];
+        let target = 8;
 
-        assert_eq!(search(input, target), 4);
+        assert_eq!(search_range(input, target), vec![3, 4]);
     }
+
     #[test]
     fn test_2() {
-        let input = vec![4, 5, 6, 7, 0, 1, 2];
-        let target = 3;
+        let input = vec![5, 7, 7, 8, 8, 10];
+        let target = 6;
 
-        assert_eq!(search(input, target), -1);
+        assert_eq!(search_range(input, target), vec![-1, -1]);
     }
 
     #[test]
     fn test_3() {
-        let input = vec![1];
+        let input = vec![];
         let target = 0;
 
-        assert_eq!(search(input, target), -1);
+        assert_eq!(search_range(input, target), vec![-1, -1]);
+    }
+
+    #[test]
+    fn test_4() {
+        let input = vec![2, 2];
+        let target = 2;
+
+        assert_eq!(search_range(input, target), vec![0, 1]);
     }
 }
