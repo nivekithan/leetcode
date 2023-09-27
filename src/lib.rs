83712bcf989fc8e5e9d7d25f9243fde3e9157542
Nivekithan S <nivekithan363@gmail.com>
2023-09-27 15:52:50 +0530

    40. Combination Sum II

--- src/lib.rs
+++ src/lib.rs
@@ -1,118 +1,41 @@
-#![allow(dead_code)]
-
-use std::cmp::Ordering;
-
-#[derive(Debug)]
-struct StateMachine {
-    candidates: Vec<i32>,
-    pub state: Vec<i32>,
-    constraint: i32,
-    cursor: usize,
-    sum: i32,
-}
-
-impl StateMachine {
-    fn new(mut candidates: Vec<i32>) -> Self {
-        candidates.sort_unstable();
-
-        return Self {
-            candidates,
-            state: Vec::new(),
-            constraint: 0,
-            cursor: 0,
-            sum: 0,
-        };
-    }
-
-    fn next(&mut self) -> Option<i32> {
-        while self.cursor < self.candidates.len() {
-            let value_under_cursor = self.candidates[self.cursor];
-
-            if !(value_under_cursor > self.constraint) {
-                self.cursor += 1;
-                continue;
-            }
-
-            self.state.push(value_under_cursor);
-            self.sum += value_under_cursor;
-
-            return Some(self.sum);
-        }
-
-        return None;
-    }
-
-    fn backtrack(&mut self) -> Result<(), ()> {
-        let first_ele = self.state.pop();
-        let second_ele = self.state.pop();
-
-        match (first_ele, second_ele) {
-            (Some(f), Some(s)) => {
-                self.sum -= f + s;
-                self.constraint = s;
-                self.cursor = 0;
-                return Ok(());
-            }
-
-            (None, None) => Err(()),
-            (None, _) => unreachable!(),
-            (Some(f), None) => {
-                self.sum = 0;
-                self.constraint = f;
-                self.cursor = 0;
-                return Ok(());
-            }
-        }
-    }
-
-    fn backtrack_1(&mut self) -> Result<(), ()> {
-        let first_ele = self.state.pop();
-
-        match first_ele {
-            Some(f) => {
-                self.sum -= f;
-                self.constraint = f;
-                self.cursor = 0;
-                return Ok(());
-            }
-            None => return Err(()),
-        };
-    }
-}
-
 struct Solution {}
 
 impl Solution {
-    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        let mut output: Vec<Vec<i32>> = vec![];
-        let mut state_machine = StateMachine::new(candidates);
-
-        loop {
-            while let Some(sum) = state_machine.next() {
-                println!("Statemachine: {state_machine:?}");
-                match sum.cmp(&target) {
-                    Ordering::Less => continue,
-                    Ordering::Equal => {
-                        let solution = state_machine.state.iter().map(|v| *v).collect::<Vec<i32>>();
-                        output.push(solution);
-                        continue;
-                    }
-                    Ordering::Greater => {
-                        let res = state_machine.backtrack();
-
-                        match res {
-                            Ok(_) => continue,
-                            Err(_) => break,
-                        };
-                    }
-                }
-            }
-
-            match state_machine.backtrack_1() {
-                Err(_) => break,
-                Ok(_) => continue,
-            }
+    fn combi(candidates: &[i32], cur_path: &mut Vec<i32>, output: &mut Vec<Vec<i32>>, target: i32) {
+        if target == 0 {
+            output.push(cur_path.clone());
+            return;
         }
+
+        for i in 0..candidates.len() {
+            let value = candidates[i];
+
+            if value > target {
+                break;
+            }
+
+            if i != 0 && value == candidates[i - 1] {
+                continue;
+            }
+
+            cur_path.push(value);
+            Self::combi(
+                &candidates[i + 1..],
+                cur_path,
+                output,
+                target - candidates[i],
+            );
+            cur_path.pop();
+        }
+    }
+
+    pub fn combination_sum2(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        candidates.sort_unstable();
+
+        let mut output: Vec<Vec<i32>> = Vec::new();
+
+        Self::combi(&candidates[0..], &mut Vec::new(), &mut output, target);
+
         return output;
     }
 }
@@ -123,42 +46,12 @@ mod test {
 
     #[test]
     fn test_1() {
-        let input = vec![2, 3, 6, 7];
-        let target = 7;
-
-        assert_eq!(
-            Solution::combination_sum(input, target),
-            vec![vec![2, 2, 3], vec![7]]
-        );
-    }
-
-    #[test]
-    fn test_2() {
-        let input = vec![2, 3, 5];
+        let input = vec![10, 1, 2, 7, 6, 1, 5];
         let target = 8;
 
         assert_eq!(
-            Solution::combination_sum(input, target),
-            vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]]
+            Solution::combination_sum2(input, target),
+            vec![vec![1, 1, 6], vec![1, 2, 5], vec![1, 7], vec![2, 6]]
         );
-    }
-
-    #[test]
-    fn test_3() {
-        let input = vec![2];
-        let target = 1;
-
-        assert_eq!(
-            Solution::combination_sum(input, target),
-            Vec::<Vec<i32>>::new()
-        );
-    }
-
-    #[test]
-    fn test_4() {
-        let input = vec![7, 3, 2];
-        let target = 18;
-
-        Solution::combination_sum(input, target);
     }
 }
