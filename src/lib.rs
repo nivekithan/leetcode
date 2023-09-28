c319b1341903753f4b1689db2db34e0a1bce5676
Nivekithan S <nivekithan363@gmail.com>
2023-09-29 04:36:00 +0530

    47. Permutations IIf

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,36 @@
+#![allow(dead_code)]
+
 struct Solution {}
 
 impl Solution {
-    fn find_perumentation(items: &[i32], output: &mut Vec<Vec<i32>>, cur: &mut Vec<i32>) {
+    fn permuate(items: &[i32], cur: &mut Vec<i32>, output: &mut Vec<Vec<i32>>) {
         if items.len() == 0 {
             output.push(cur.clone());
+            return;
         }
 
         for i in 0..items.len() {
-            let v = items[i];
+            let value = items[i];
 
-            let mut new_array: Vec<i32> = Vec::new();
+            if i != 0 && value == items[i - 1] {
+                continue;
+            }
 
-            new_array.extend_from_slice(items);
-            new_array.remove(i);
+            let mut new_items = Vec::new();
 
-            cur.push(v);
-            Self::find_perumentation(&new_array[0..], output, cur);
+            new_items.extend_from_slice(items);
+            new_items.remove(i);
+
+            cur.push(value);
+
+            Self::permuate(&new_items, cur, output);
             cur.pop();
         }
     }
-
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut output: Vec<Vec<i32>> = Vec::new();
-
-        Self::find_perumentation(&nums[0..], &mut output, &mut vec![]);
-
+    pub fn permute_unique(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort_unstable();
+        let mut output = Vec::new();
+        Self::permuate(&nums, &mut vec![], &mut output);
         return output;
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::Solution;
-
-    #[test]
-    fn test_1() {
-        let input = vec![1, 2, 3];
-
-        assert_eq!(
-            Solution::permute(input),
-            vec![
-                vec![1, 2, 3],
-                vec![1, 3, 2],
-                vec![2, 1, 3],
-                vec![2, 3, 1],
-                vec![3, 1, 2],
-                vec![3, 2, 1]
-            ]
-        )
     }
 }
